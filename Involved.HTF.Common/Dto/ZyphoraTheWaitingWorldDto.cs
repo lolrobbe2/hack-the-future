90d284d6efcbd7ab2d18eb2b3788db2a60d76d58
Beernaert Robbe <robbe.beernaert@student.howest.be>
2024-11-19 09:39:58 +0100

    first commit

---
+++ Involved.HTF.Common/Dto/ZyphoraTheWaitingWorldDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Involved.HTF.Common.Dto;
+
+public class ZyphoraTheWaitingWorldDto
+{
+    /// <summary>
+    ///     Time on the planet
+    /// </summary>
+    public DateTime SendDateTime { get; set; }
+
+    /// <summary>
+    ///     The speed at which the message travels in lightYears/minute
+    /// </summary>
+    public int TravelSpeed { get; set; }
+
+    /// <summary>
+    ///     The distance between the planet and the base station in light years
+    /// </summary>
+    public int Distance { get; set; }
+
+    /// <summary>
+    ///     The nr of hours in a day on the planet
+    /// </summary>
+    public int DayLength { get; set; }
+}
---
+++ Involved.HTF.Common/HackTheFutureClient.cs
@@ -0,0 +1,18 @@
+﻿namespace Involved.HTF.Common;
+
+public class HackTheFutureClient : HttpClient
+{
+    public HackTheFutureClient()
+    {
+        BaseAddress = new Uri("https://app-htf-2024.azurewebsites.net/");
+    }
+
+    public async Task Login(string teamname, string password)
+    {
+        var response = await GetAsync($"/api/team/token?teamname={teamname}&password={password}");
+        if (!response.IsSuccessStatusCode)
+            throw new Exception("You weren't able to log in, did you provide the correct credentials?");
+        var token = await response.Content.ReadAsStringAsync();
+        DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+    }
+}
